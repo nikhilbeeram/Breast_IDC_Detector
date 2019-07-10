08771e139adf72a90ae9394bac8964aa256e3e3f
Nimble AIML Techniques <52631229+nikhilbeeram@users.noreply.github.com>
2019-07-10 18:14:01 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/file/d/1OYvq4gaDVFCWm_ZgVrLaH3Hn_eSuIUmu/view?usp=sharing'
 export_file_name = 'export.pkl'
 
-classes = ['NORMAL', 'PNEUMONIA']
+classes = ['NORMAL','PNEUMONIA']
 path = Path(__file__).parent
 
 app = Starlette()
