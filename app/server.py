67df1becfb84555549d850bdf40e9aa0dd4ada1b
Nimble AIML Techniques <52631229+nikhilbeeram@users.noreply.github.com>
2019-07-10 18:24:19 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/18I5cT4JZ8f9Xc4c7eEK6_2QFx3RJ-Dea/view?usp=sharing'
-export_file_name = 'model_81_accuracy.pkl'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
 
 classes = ['NORMAL','PNEUMONIA']
 path = Path(__file__).parent
